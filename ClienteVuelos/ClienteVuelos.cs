65858c34dca1c1747ffb60fbf61788004d61c219
TLEMEX <TLEMEX@LAPTOP-1PVF4VVF>
2020-09-29 22:53:56 -0500

    Aplicacion cliente de vuelos

---
+++ ClienteVuelos/ClienteVuelos.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace ClienteVuelos
+{
+    public class ClienteVuelo
+    {
+        public delegate void Movimientos();
+        public event Movimientos AlHaberMovimientos;
+        HttpClient cliente = new HttpClient();
+        public ClienteVuelo()
+        {
+            cliente.BaseAddress=new Uri("http://vuelos.itesrc.net");
+        }
+
+        public async void Agregar(DatosVuelos vuelos)
+        {
+            var json = JsonConvert.SerializeObject(vuelos);
+            var result = await cliente.PostAsync("/Tablero", new StringContent(json, Encoding.UTF8, "application/json"));
+            result.EnsureSuccessStatusCode();
+        }
+        public async void Editar(DatosVuelos vuelos)
+        {
+            var json = JsonConvert.SerializeObject(vuelos);
+            var result = await cliente.PutAsync("/Tablero", new StringContent(json, Encoding.UTF8, "application/json"));
+            result.EnsureSuccessStatusCode();
+        }
+        public async void Eliminar(DatosVuelos vuelos)
+        {
+            var json = JsonConvert.SerializeObject(vuelos);
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Delete, "/Tablero");
+            message.Content = new StringContent(json, Encoding.UTF8, "application/json");
+            var result = await cliente.SendAsync(message);
+            result.EnsureSuccessStatusCode();
+        }
+        public IEnumerable<DatosVuelos> Modelo { get; set; }
+        public async void Get()
+        {
+            
+            var client = new HttpClient();
+            var response = await client.GetAsync("http://vuelos.itesrc.net/Tablero");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                Modelo = JsonConvert.DeserializeObject<IEnumerable<DatosVuelos>>(jsonString);
+                AlHaberMovimientos?.Invoke();
+            }
+        }
+    }
+}
---
+++ ClienteVuelos/MainWindow.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ClienteVuelos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = dv;
+            cv.Get();
+            cv.AlHaberMovimientos += Cv_AlHaberMovimientos;
+            cmbEstados.Items.Add("A TIEMPO");
+            cmbEstados.Items.Add("ABORDANDO");
+            cmbEstados.Items.Add("RETRASADO");
+            cmbEstados.Items.Add("CANCELADO");
+            cronometro.Interval = TimeSpan.FromSeconds(2);
+            cronometro.Tick += Cronometro_Tick;
+            cronometro.Start();
+        }
+
+        private void Cronometro_Tick(object sender, EventArgs e)
+        {
+            cv.Get();
+        }
+
+        private void Cv_AlHaberMovimientos()
+        {
+            lsvVuelos.ItemsSource = cv.Modelo;
+        }
+        DispatcherTimer cronometro = new DispatcherTimer();
+        DatosVuelos dv = new DatosVuelos();
+        DatosVuelos dv2 = new DatosVuelos();
+        ClienteVuelo cv = new ClienteVuelo();
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                cv.Agregar(dv2);
+                btnAgregar.IsEnabled = false;
+                txtVuelo.IsEnabled = txtHora.IsEnabled = txtDestino.IsEnabled = false;
+                txtDestino.Text = txtHora.Text = txtVuelo.Text = cmbEstados.Text = "";
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnEditar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                dv.Hora = txtHora.Text;
+                dv.Vuelo = txtVuelo.Text;
+                dv.Destino = txtDestino.Text;
+                dv.Estado = cmbEstados.Text;
+                cv.Editar(dv);
+                cv.Get();
+                cronometro.Start();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            if (lsvVuelos.SelectedIndex != -1)
+            {
+            try
+            {
+                    DatosVuelos dvs = new DatosVuelos();
+                    dvs=lsvVuelos.SelectedItem as DatosVuelos;
+                    if (MessageBox.Show($"El vuelo {dvs.Vuelo} está a punto de ser eliminado. ¿Desea continuar?", "Atencion",
+                        MessageBoxButton.OKCancel, MessageBoxImage.Warning) == MessageBoxResult.OK)
+                    {
+                        cv.Eliminar(dv);
+                        txtDestino.Text = txtHora.Text = txtVuelo.Text = cmbEstados.Text = "";
+                        cv.Get();
+                        cronometro.Start();
+                    }  
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            }
+            else
+            {
+                MessageBox.Show("Es necesario que se seleccione un vuelo para ser eliminado.", "Atencion", MessageBoxButton.OK);
+            }
+        }
+        private void btnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = dv2;
+            btnAgregar.IsEnabled = true;
+            txtDestino.IsEnabled = true;
+            txtHora.IsEnabled = true;
+            txtVuelo.IsEnabled = true;
+        }
+
+        private void lsvVuelos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lsvVuelos.SelectedItem != null)
+            {
+                cronometro.Stop();
+                dv = lsvVuelos.SelectedItem as DatosVuelos;
+                txtHora.Text = dv.Hora;
+                txtDestino.Text = dv.Destino;
+                txtVuelo.Text = dv.Vuelo;
+                cmbEstados.Text = dv.Estado;
+            }
+        }
+    }
+}
